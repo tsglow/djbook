314ca234c41ef512f8d112dd5aea0adc144c25a6
dinner_rolls <haruki84@msn.com>
2026-05-14 08:33:35 +0900

    feat: added templates and default views

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
+
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
+    author = models.CharField(null=True,max_length=100)
+    is_bestselling = models.BooleanField(default=False)    
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
+    
+    
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from book_outlet.models import Book
+from django.utils.text import slugify
 
 # Create your views here.
+
+def dbtodict():
+    list = Book.objects.all()
+    book_list = []
+    for b in list:        
+        book_list.append({
+            "title": b.title,
+            "author": b.author,
+            "rating": b.rating,
+            "is_bestselling": b.is_bestselling,
+            "url": slugify(b.title)
+            })
+    return book_list
+
+def get_detail(url):
+    book_list = dbtodict()
+    result = next((book for book in book_list if book["url"] == url))    
+    return result
+
+
+def index(request):
+    return render(request,"book_outlet/index.html", {"books": dbtodict()})
+
+def detail(request,url):
+    return render(request, "book_outlet/detail.html", {"book": get_detail(url)})
