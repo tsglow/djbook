1dbb65926a526a03c4db3246c8642d4dbf1af2e4
dinner_rolls <haruki84@msn.com>
2026-05-14 16:16:49 +0900

    feat: custom admin model

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Book
 
 # Register your models here.
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):    
+    prepopulated_fields = {"url": ("title",) }
+    list_filter = ("author", "rating")
+    list_display = ("title", "author")
+
+admin.site.register(Book, BookAdmin)
